2f2a2999cb2932dbeb9d83cf7deb86ba564fc623
epandonez <heebcomponents@gmail.com>
2023-05-05 20:39:58 +0100

    Changes to the stereoiser

--- Doofuzz_Stereoiser.h
+++ Doofuzz_Stereoiser.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <limits>
 #include "iir1/Iir.h"
 
 class Stereoiser {
@@ -12,8 +13,9 @@ public:
   inline double reset(const double _sampleRate) {
 
     m_HighPass.setup(_sampleRate, kHighPassF);
+    m_LowPass .setup(_sampleRate, kLowPassF );
 
-    double notchFreq = 20000.0;
+    double notchFreq = 4000.0 * kSqrtPhi;
 
     for (int n = 0; n < kNumNotches; n++) {
       for (int ch = 0; ch < 2; ch++) {
@@ -24,16 +26,24 @@ public:
     return _sampleRate;
   }
 
-  inline double setWidth(double _width) {
-    return m_Width = _width;
-  }
+  inline double setWidth(double _width) {                   // 0.0..1.0; the stored and used
+    m_WidthSquared = std::clamp(_width * _width, 0.0, 1.0); // value is actually the square,
+    return _width;                                          // in order to make the control
+  }                                                         // feel smoother.
+
 
   inline void  processFrame(const sample  inputL,
                             const sample  inputR,
                             sample*       outputL,
                             sample*       outputR) {
 
-    if (m_Width != 0.0) {
+ 
+    if (m_WidthSquared < kEpsilon) {
+
+      *outputL  = inputL;
+      *outputR  = inputR;
+
+    } else {
 
       double processed[2] = { inputL, inputR };
 
@@ -43,15 +53,14 @@ public:
         }
       }
 
-      double processedCombinedAndFiltered = m_Width * m_HighPass.filter(processed[0] - processed[1]);
+      double processedCombinedAndFiltered = kWidthMultiplier *
+                                              m_WidthSquared *
+                                                m_LowPass.filter(
+                                                  m_HighPass.filter(
+                                                    processed[0] - processed[1]));
 
       *outputL  = inputL + processedCombinedAndFiltered; // inputL;
       *outputR  = inputR - processedCombinedAndFiltered; // inputR;
-
-    } else {
-
-      *outputL  = inputL;
-      *outputR  = inputR;
 
     }
 
@@ -59,15 +68,22 @@ public:
 
 private:
 
-  static const  inline  double  kHighPassF  = 250.0;
-  static const  inline  int     kNumNotches = 10;
-  static const  inline  double  kNotchQ     = 7.5; // 10.0;
-  static const  inline  double  kPhi        = (sqrt(5.0) + 1.0) / 2.0;
-  static const  inline  double  kSqrtPhi    = sqrt(kPhi);
+  static  const inline  double  kEpsilon          = std::numeric_limits<double>::epsilon();
 
-  double                        m_Width     = 1.0;
+  static  const inline  double  kHighPassF        =  250.0;
+  static  const inline  double  kLowPassF         = 4000.0;
+
+  static  const inline  int     kNumNotches       =  7;
+  static  const inline  double  kNotchQ           = 10.0;
+  static  const inline  double  kPhi              = (sqrt(5.0) + 1.0) / 2.0;
+  static  const inline  double  kSqrtPhi          = sqrt(kPhi);
+
+  static  const inline  double  kWidthMultiplier  = 2.0;
+
+  double                        m_WidthSquared    = 1.0;
 
   Iir::RBJ::IIRNotch            m_Notch[2][kNumNotches];  // channels, notches
   Iir::Butterworth::HighPass<1> m_HighPass;
+  Iir::Butterworth::LowPass <1> m_LowPass;
 
 };
